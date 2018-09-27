a6768845ef5ec0a28a700dade3febc01f5d25239
Israel Vargas Merljak <imerljak@gmail.com>
2018-09-27 18:58:18 -0300

    corrigindo data.sql p/ evitar exceções.. alterado ddl-auto p/ update.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,9 @@
 -- Initialize cargos
-insert into cargo values ('ROLE_REVISOR_RELATOR', 1, 'Revisor/Relator');
-insert into cargo values ('ROLE_GERENTE', 2, 'Gerente');
-insert into cargo values ('ROLE_ADMINISTRADOR', 3, 'Administrador');
+insert into cargo values ('ROLE_REVISOR_RELATOR', 1, 'Revisor/Relator')
+on conflict do nothing;
+
+insert into cargo values ('ROLE_GERENTE', 2, 'Gerente')
+on conflict do nothing;
+
+insert into cargo values ('ROLE_ADMINISTRADOR', 3, 'Administrador')
+on conflict do nothing;
