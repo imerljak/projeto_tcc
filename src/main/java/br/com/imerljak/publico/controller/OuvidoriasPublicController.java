8d6e47348f613952220bb6553231ace476d2ff25
Israel Vargas Merljak <imerljak@gmail.com>
2018-12-12 09:33:16 -0200

    finalizado cadastro de acesso público p/ ouvidorias. finalizado upload de arquivos p/ ouvidorias (publico e admin) + validação documento informado. + atualizando lista de ouvidorias após registro

--- src/main/java/br/com/imerljak/publico/controller/OuvidoriasPublicController.java
+++ src/main/java/br/com/imerljak/publico/controller/OuvidoriasPublicController.java
@@ -1,17 +1,22 @@
 package br.com.imerljak.publico.controller;
 
 
+import br.com.caelum.stella.validation.Validator;
 import br.com.imerljak.concessionarias.model.Concessionaria;
 import br.com.imerljak.concessionarias.service.ConcessionariaRepository;
 import br.com.imerljak.ouvidorias.model.AnexoOuvidoria;
 import br.com.imerljak.ouvidorias.model.Ouvidoria;
 import br.com.imerljak.ouvidorias.service.OuvidoriaRepository;
+import br.com.imerljak.ouvidorias.value.TipoDocumentoPessoa;
 import br.com.imerljak.processos.model.Processo;
 import br.com.imerljak.processos.service.ProcessoRepository;
+import br.com.imerljak.shared.model.Protocolo;
 import br.com.imerljak.utils.FileUploader;
 import br.com.imerljak.utils.UploadPath;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -21,7 +26,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotNull;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/cidadao")
 @Controller
@@ -51,13 +59,28 @@ public class OuvidoriasPublicController {
     @GetMapping("/ouvidorias")
     @ResponseBody
     public Page<Ouvidoria> ouvidoriasPage(@PageableDefault Pageable pageable) {
-        return ouvidoriaRepository.findAll(pageable);
+
+        final PageRequest pageRequest = PageRequest.of(
+                pageable.getPageNumber(),
+                pageable.getPageSize(),
+                Sort.Direction.DESC, "id");
+
+        return ouvidoriaRepository.findAll(pageRequest);
     }
 
     @PostMapping(value = "/addOuvidoria")
     @ResponseBody
-    public ResponseEntity<Ouvidoria> postOuvidoria(Ouvidoria ouvidoria, MultipartFile[] files) {
+    public ResponseEntity<?> postOuvidoria(Ouvidoria ouvidoria, MultipartFile[] files) {
         System.out.println(ouvidoria.getTipo());
+
+        @NotNull final TipoDocumentoPessoa tipoDocumento = ouvidoria.getCidadao().getTipoDocumento();
+        final Validator<String> validator = tipoDocumento.getValidator();
+
+        if (!isValido(validator, ouvidoria.getCidadao().getDocumento())) {
+            System.out.println("INVALIDO!");
+            return ResponseEntity.badRequest()
+                    .body(Collections.singletonMap("mensagem", tipoDocumento.name() + " inválido."));
+        }
 
         FileUploader.save(files, UploadPath.OUVIDORIAS);
 
@@ -70,8 +93,18 @@ public class OuvidoriasPublicController {
 
         ouvidoriaRepository.save(ouvidoria);
 
+        Map<String, Protocolo> protocoloMap = Collections.singletonMap("protocolo", ouvidoria.getProtocolo());
+
         return ResponseEntity.accepted()
-                .body(ouvidoria);
+                .body(protocoloMap);
+    }
+
+    private boolean isValido(Validator<String> validator, @NotNull String documento) {
+        if (!validator.isEligible(documento)) {
+            return false;
+        }
+
+        return validator.invalidMessagesFor(documento).isEmpty();
     }
 
     @GetMapping("/processos")
